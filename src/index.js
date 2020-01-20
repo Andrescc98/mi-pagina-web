6a9390b7444a7e8598c4aed7307562bbf9a8abd5
Andrescc98 <andresjosecc@gmail.com>
2020-01-20 10:15:04 -0400

    agregando motor de plantillas ejs

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 const express=require('express');
 const morgan=require('morgan');
+const path=require('path');
 
 const app=express();
 
 //settings
 app.set('port', 3000);
+app.set('views',path.join(__dirname,'views'));
+app.engine('html',require('ejs').renderFile);
+app.set('view engine','ejs');
 
 //middlewares
 app.use(morgan('dev'));
 
 //routes
 app.use(require('./routes'));
+
+//public
+app.use(express.static(path.join(__dirname,'public')));
 
 //server port
 app.listen(app.get('port'),()=>{
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ const express=require('express');
 const router=express.Router();
 
 router.get('/',(req,res)=>{
-    res.send('hola mundo');
+    res.render('index.html');
 });
 
 module.exports=router;
